38ba866d2bc2ce95f1249e28bb5e62323029cb79
Kasim Siddiqui <kasimsiddiqui@gmail.com>
2015-09-09 14:05:56 -0700

    initial

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulpMocha = require('gulp-mocha');
 var watch = require('gulp-watch');
 
 gulp.task('jshint', function() {
-  return gulp.src(['index.js', 'bin/greet', 'test/**/*test.js', 'lib/**/*.js', 'gulpfile.js'])
+  return gulp.src(['index.js', 'test/**/*test.js', 'lib/**/*.js', 'gulpfile.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
@@ -17,7 +17,7 @@ gulp.task('test', function() {
 });
 
 gulp.task('watch', function () {
-  return gulp.watch(['index.js', 'bin/greet', 'test/**/*test.js', 'lib/**/*.js'], ['jshint']);
+  return gulp.watch(['index.js', 'test/**/*test.js', 'lib/**/*.js'], ['jshint']);
 });
 
 gulp.task('default', ['jshint', 'test']);
