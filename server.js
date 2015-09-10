f34eae51ed34b128054b2a55965af64f92289a09
Kasim Siddiqui <kasimsiddiqui@gmail.com>
2015-09-10 09:10:10 -0700

    hw

--- server.js
+++ server.js
@@ -4,22 +4,39 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
+  var data = '';
   if (req.method === 'GET') {
     res.writeHead(200, {'Content-Type': 'application/json'});
-    fs.readdir('./data', function(err, files) {
-      res.write(JSON.stringify(files));
-      res.end();
-    });
+      var match = req.url.match(/\/name\/(\d+)/);
+      if(match){
+        var num = match[1];
+        fs.readFile('./data/' + num + '.json', function(err, data){
+          res.write(data);
+          res.end();
+        })
+      } else {
+        fs.readdir('./data', function(err, files) {
+          files = files.map(function(data){
+            return data.match(/^(\d+)\./)[1];
+          })
+          .sort(function(b, a){
+            return b - a;
+          });
+        res.write(JSON.stringify(files));
+        res.end();
+        });
+      }
   } else if (req.method === 'POST') {
-    console.log(JSON.stringify(res.body));
-    res.on('data', function(chunk) {
-      console.log(chunk);
+    req.on('data', function(chunk) {
+      data += chunk;
     });
-    fs.readdir('./data', function(err, files) {
-      var fileNum = files.length + 1;
-      fs.writeFile('data/' + fileNum + '.json', req.body, function(err) {
-        if (err) throw err;
-        console.log('file saved');
+    req.on('end', function(){
+      fs.readdir('./data', function(err, files) {
+        var fileNum = files.length + 1;
+        fs.writeFile('data/' + fileNum + '.json', data, function(err) {
+          if (err) throw err;
+          console.log('file saved');
+        });
       });
     });
   } else {
--- test/server_test.js
+++ test/server_test.js
@@ -10,5 +10,22 @@ require('../server');
 chai.use(chaiHttp);
 
 describe('server.js', function() {
+  it('status code 200', function(done) {
+    chai.request('localhost:3000')
+      .get('/')
+      .then(function(res) {
+        expect(res.statusCode).to.eql(200);
+        done();
+      });
+  });
+
+  it('respond with JSON object', function(done) {
+  chai.request('localhost:3000')
+    .get('/')
+    .then(function(res) {
+      expect(typeof(res.body)).to.equal('object');
+      done();
+    });
+  });
 
 });
